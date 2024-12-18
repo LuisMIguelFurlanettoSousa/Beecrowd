e64bd2b14b48b89ccaf1ef327b05f20cdcaa711b
Luís Miguel <148079567+LuisMIguelFurlanettoSousa@users.noreply.github.com>
2024-12-18 13:41:23 -0300

    ex

--- Python/4-ESTRUTURAS-E-BIBLIOTECAS/1022.py
+++ Python/4-ESTRUTURAS-E-BIBLIOTECAS/1022.py
@@ -1,26 +1,40 @@
-rep = int(input())
+from math import gcd
 
-for _ in range(rep):
-    tokens = input().split()
-
-    numeros = [int(x) if x.isdigit() else x for x in tokens]
-    
-    n2 = numeros[0]
-    d1 = numeros[2]
-    n2 = numeros[4]
-    d2 = numeros[6]
-    operador = numeros[3]
+def simplificar(n, d):
+    divisor_comum = gcd(n, d)
+    return n // divisor_comum, d // divisor_comum
 
 
-    Sub = (n2*d2 - n2*d1) / (d1*d2)
-    Multi = (n2*n2) / (d1*d2)
-    Div = (n2*d2)/(n2*d1)
+def main():
+    n = int(input())
 
-    if operador == '+':
-        Soma =  (n2*d2 + n2*d1) / (d1*d2)
-        print(Soma)
+    for _ in range(n):
+        n1, _, d1, operacao, n2, _, d2 = input().split()
+        n1, d1, n2, d2 = int(n1), int(d1), int(n2), int(d2)
 
+        if operacao == '+':
+            numerador = (n1 * d2) + (n2 * d1)
+            denominador = d1 * d2
+        elif operacao == '-':
+            numerador = (n1 * d2) - (n2 * d1)
+            denominador = d1 * d2
+        elif operacao == '*':
+            numerador = n1 * n2
+            denominador = d1 * d2
+        elif operacao == '/':
+            numerador = n1 * d2
+            denominador = n2 * d1
 
+        if denominador < 0:
+            numerador = -numerador
+            denominador = -denominador
 
+        resultado_nao_simplificado = f"{numerador}/{denominador}"
 
-    #1 / 2 + 3 / 4
+        num_simplificado, den_simplificado = simplificar(numerador, denominador)
+        resultado_simplificado = f"{num_simplificado}/{den_simplificado}"
+
+        print(f"{resultado_nao_simplificado} = {resultado_simplificado}")
+        
+if __name__ == "__main__":
+    main()
